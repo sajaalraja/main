ec76676046a59899381fd6ba1ba90860954d8445
sajaalraja <>
2021-04-14 15:00:10 +0000

    updated

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var typecomputer= prompt('please enter type of acomputer (dell, hp,acer)?')
+if(typecomputer=='dell'){
+  console.log('dell company')   
+}
+else if(typecomputer=='hp'){
+
+  console.log( 'hp company')
+}
+else if(typecomputer=='acer')
+{
+  console.log('acer company')
+} 
+else {
+  console.log ('not found')
+}
+
