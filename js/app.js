4fb03e896c5ca34b74c8c20f6a1325edf63f02ab
sajaalraja <>
2021-04-15 13:18:20 +0000

    updated

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
-var typecomputer= prompt('please enter type of acomputer (dell, hp,acer)?')
-if(typecomputer=='dell'){
-  console.log('dell company')   
+var typecomputer= prompt('please enter type of acomputer (dell, acer)?')
+while (typecomputer !=='dell'   &&typecomputer!=='acer') {
+  typecomputer= prompt('please enter typecomputer ( dell,acer)?');
+  console.log(typecomputer);
 }
-else if(typecomputer=='hp'){
+var computerNumber = prompt('how many computer do you want to order?');
+for (var i = 1; i <=computerNumber; i++){
 
-  console.log( 'hp company')
+
+
+
+if(typecomputer=='dell'){
+ var image = '<img src="js/img/dell.jpg" alt= "dell"/>';
+   
+    document.write(image);
+  
 }
+
 else if(typecomputer=='acer')
 {
-  console.log('acer company')
+ var image2 = '<img src="js/img/acer.jpg" alt="dell"/>';
+   
+    document.write(image2);
 } 
-else {
-  console.log ('not found')
-}
 
+
+}
